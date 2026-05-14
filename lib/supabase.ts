c0962e057c52c7e7fc8bbf4216fa42af7c0ab873
안소정 <hectoinno0325@gmail.com>
2026-05-14 16:47:51 +0900

    fix supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export type Employee = {
+  id: number
+  name: string
+}
